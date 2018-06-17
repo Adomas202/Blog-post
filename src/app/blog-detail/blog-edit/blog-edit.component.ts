8f2a718512b1f0af7786e3c94f2218c3b7ecead7
Poleninas <adomas202@gmail.com>
2018-06-17 11:41:56 +0300

    blog edit component works and saves chaned data
    Now the blog esit component is working, it aldo displays data with ngModel and cna save data to localStorag

--- src/app/blog-detail/blog-edit/blog-edit.component.ts
+++ src/app/blog-detail/blog-edit/blog-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Post} from '../../post';
+import { DataService } from '../../data.service';
+import {post} from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-blog-edit',
@@ -6,14 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-edit.component.css']
 })
 export class BlogEditComponent implements OnInit {
+  public id: number;
+  public name: string;
+  text: string;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
-  ngOnInit() {
+  public ngOnInit() {
+    this.data.currentPost.subscribe(id => this.id = id);
+    this.data.changeSelectedId(this.id);
+    let posts = this.getPosts();
+    posts = posts.filter((post)=>post.id == this.id);
+    this.name = posts[0].name;
+    this.text = posts[0].text;
   }
 
-  onPostEdited() {
-    
+  onPostEdited(name: string, text: string) {
+    let posts = this.getPosts();
+    posts[this.id].name = name;
+    posts[this.id].text = text;
+    localStorage.setItem('posts', JSON.stringify({posts: posts}));
+  }
+  //
+  // private setLocalStoragePosts(posts: Post[]): void {
+  //   localStorage.setItem('posts', JSON.stringify({posts: posts}));
+  // }
+
+  public getPosts(): Post[] {
+    let localStorageItem = JSON.parse(localStorage.getItem('posts'));
+    return localStorageItem == null ? [] : localStorageItem.posts;
   }
 
 }
