9195062e01cd522236cff37daf8cc39cf6d169d6
Poleninas <adomas202@gmail.com>
2018-06-18 16:06:11 +0300

    Parentheses fixed
    In data service parentheses were not cloesd

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,12 +10,15 @@ export class DataService {
   private selectedPost = new BehaviorSubject<number>(1);
   currentPost = this.selectedPost.asObservable();
 
-  constructor() { }
+  constructor() {
+  }
 
   changeSelectedId(id: number) {
     this.selectedPost.next(id);
-}
+  }
+
   public getPosts(): Post[] {
     let localStorageItem = JSON.parse(localStorage.getItem('posts'));
     return localStorageItem == null ? [] : localStorageItem.posts;
   }
+}
